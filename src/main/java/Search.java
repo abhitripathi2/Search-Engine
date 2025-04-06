b5673f512aacadbba4ac9913aea8158852cbca59
abhit <abhitriangle@gmail.com>
2025-04-06 16:16:59 +0530

    Commit

---
+++ src/main/java/Search.java
@@ -0,0 +1,7 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/Search")
+public class Search extends HttpServlet {
+    protected void doGet()
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<form action = "Search">
+    <input type = "text"></input>
+    <button type = "submit">Search</button>
+</form>
+</body>
+</html>
